35ce83a9695c276ca0818ee9ee9dd96a63a4d946
Hade21 <abdurrohmanmuhammad123@gmail.com>
2022-01-24 08:51:05 +0800

    fix some bugs when input is not number it shows NaN

--- script.js
+++ script.js
@@ -10,55 +10,53 @@ const customTip = document.querySelector('.custom-input')
 
 function tipAmount(value) {
     if (bill.value != 0 && person.value != 0 && value != 0) {
+        bill.style.border = 'none';
+        person.style.border = 'none';
         return [(((bill.value * value) / 100) / person.value), (bill.value / person.value)];
     } else if ((bill.value == 0 || bill.value == '') && (person.value == 0 || person.value == '')) {
         erorBill.innerText = 'Can\'t be zero';
         erorPeople.innerText = 'Can\'t be zero';
-        return [0, 0];
-    } else if ((typeof bill.value == String) && (typeof person.value == String)) {
-        erorBill.innerText = 'Unvalid bill';
-        erorPeople.innerText = 'Unvalid bill';
+        bill.style.outline = 'none';
+        person.style.outline = 'none';
+        bill.style.border = '2px solid red';
+        person.style.border = '2px solid red';
         return [0, 0];
     } else if (person.value == 0 || person.value == '') {
+        person.style.border = '2px solid red';
+        person.style.outline = 'none';
         erorPeople.innerText = 'Can\'t be zero';
         return [0, 0];
-    } else if (typeof person.value == String) {
-        erorPeople.innerText = 'Unvalid bill';
-        return [0, 0];
     } else if (bill.value == 0 || bill.value == '') {
+        bill.style.border = '2px solid red';
+        bill.style.outline = 'none';
         erorBill.innerText = 'Can\'t be zero';
-        return [0, 0];
-    } else if (typeof bill.value == String) {
-        erorBill.innerText = 'Unvalid bill';
         return [0, 0];
     }
 }
 
 function custom(value) {
     if (Number(value.value) != 0 || Number(value.value) != '') {
+        bill.style.border = 'none';
+        person.style.border = 'none';
+        customTip.style.border = 'none';
         return [(((bill.value * value.value) / 100) / person.value), (bill.value / person.value)]
-    } else if (Number(value.value) == NaN) {
-        customTip.style.border = '2px solid red';
-        return [0, 0];
     } else if ((bill.value == 0 || bill.value == '') && (person.value == 0 || person.value == '')) {
         erorBill.innerText = 'Can\'t be zero';
         erorPeople.innerText = 'Can\'t be zero';
-        return [0, 0];
-    } else if ((typeof bill.value == String) && (typeof person.value == String)) {
-        erorBill.innerText = 'Unvalid bill';
-        erorPeople.innerText = 'Unvalid bill';
+        bill.style.outline = 'none';
+        person.style.outline = 'none';
+        bill.style.border = '2px solid red';
+        person.style.border = '2px solid red';
         return [0, 0];
     } else if (person.value == 0 || person.value == '') {
+        person.style.border = '2px solid red';
+        person.style.outline = 'none';
         erorPeople.innerText = 'Can\'t be zero';
         return [0, 0];
-    } else if (typeof person.value == String) {
-        erorPeople.innerText = 'Unvalid bill';
-        return [0, 0];
     } else if (bill.value == 0 || bill.value == '') {
+        bill.style.border = '2px solid red';
+        bill.style.outline = 'none';
         erorBill.innerText = 'Can\'t be zero';
-        return [0, 0];
-    } else if (typeof bill.value == String) {
-        erorBill.innerText = 'Unvalid bill';
         return [0, 0];
     }
 }
@@ -69,14 +67,20 @@ buttons.forEach(function(btn) {
             btnSelect.className = 'btn-input';
             erorBill.innerText = '';
             erorPeople.innerText = '';
+            customTip.style.border = 'none';
         })
         btn.classList.add('btn-active');
 
         let variable = 0;
         variable = (Number((Array.from(event.target.value)).filter((el, i) => i < event.target.value.length - 1).join('')));
 
-        totalTip.innerText = tipAmount(variable)[0].toFixed(2);
-        totalBill.innerText = tipAmount(variable).reduce((acc, cur) => acc + cur).toFixed(2);
+        if (String(tipAmount(variable)[0]) == 'NaN') {
+            totalTip.innerText = '0.00';
+            totalBill.innerText = '0.00';
+        } else {
+            totalTip.innerText = tipAmount(variable)[0].toFixed(2);
+            totalBill.innerText = tipAmount(variable).reduce((acc, cur) => acc + cur).toFixed(2);
+        }
     })
 })
 
@@ -90,6 +94,7 @@ reset.addEventListener('click', function() {
     totalBill.innerText = '0.00';
     bill.value = '';
     person.value = '';
+    customTip.value = '';
 })
 
 customTip.addEventListener('input', function() {
@@ -98,7 +103,11 @@ customTip.addEventListener('input', function() {
         erorBill.innerText = '';
         erorPeople.innerText = '';
     })
-    totalTip.innerText = custom(customTip)[0].toFixed(2);
-    totalBill.innerText = custom(customTip).reduce((acc, cur) => acc + cur).toFixed(2);
+    if (String(custom(customTip)[0]) == 'NaN') {
+        totalTip.innerText = '0.00';
+        totalBill.innerText = '0.00';
+    } else {
+        totalTip.innerText = custom(customTip)[0].toFixed(2);
+        totalBill.innerText = custom(customTip).reduce((acc, cur) => acc + cur).toFixed(2);
+    }
 })
-console.log(Number(customTip.value))
